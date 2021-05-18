4c19c63da0d4093fd00ddb3c8348b3dd46736b84
knightV <9569076+dxv2k@users.noreply.github.com>
2021-05-18 23:50:00 +0700

    done LAB02

--- LAB02_threejs/js/main.js
+++ LAB02_threejs/js/main.js
@@ -1,16 +1,91 @@
-
 // console.log(THREE); 
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 
-    45, 
-    window.innerWidth / window.innerHeight, 
-    1, 
-    1000 );
+function init(){ 
+    var scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    // Add fog to scene 
+    scene.fog = new THREE.FogExp2(0xfffff, 0.2); // param: color, density
+
+    var box = getBox(1,1,1);
+    var plane = getPlane(20);
+
+    // Set object as name and we can find it, call it by name
+    plane.name = 'plane-1'; 
+
+    box.position.y = box.geometry.parameters.height/2; // move box on top of plane  
+    plane.rotation.x = Math.PI/2; // view at 90 degree angle 
+    // plane.rotation.y = 1;  
+
+    scene.add(box); 
+    // plane.add(box); 
+    scene.add(plane); 
+
+    var camera = new THREE.PerspectiveCamera( 
+        45, // field of view 
+        window.innerWidth / window.innerHeight, // aspect ratio 
+        // Distance can be seen
+        1, // near clipping planes 
+        1000 // far clipping planes 
+        );
+
+    // Set up camera position manually 
+    camera.position.x = 1;   
+    camera.position.y = 2;   
+    camera.position.z = 5;   
+    // use lookAt to set camera position 
+    camera.lookAt(new THREE.Vector3(0,0,0)); 
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // renderer.setClearColor(0xffff); // set background color 
+    // use css style for color setting 
+
+    renderer.setClearColor('rgb(255,255,255)'); // set background color 
+    // document.body.appendChild( renderer.domElement );
+    document.getElementById('webgl').appendChild(renderer.domElement); 
+    renderer.render(scene, camera);
+    update(renderer,scene,camera); 
+    return scene; 
+}
+
+function update(renderer, scene, camera){ 
+    renderer.render(scene, camera);
+
+    // // Call object by name
+    // var plane = scene.getObjectByName('plane-1');
+    // // Rotation animation 
+    // plane.rotation.y += 0.001; 
+    // plane.rotation.z += 0.001; 
+
+    // // traverse by scaling child objecet
+    // scene.traverse(function(child){ 
+    //     child.scale.x += 0.001; 
+    // }); 
+
+    requestAnimationFrame(function(){    
+        update(renderer, scene, camera); 
+    }); // get callback in recursive manner
+}
+
+function getBox(w, h, d){ 
+    var geometry = new THREE.BoxGeometry(w,h,d); 
+    var material = new THREE.MeshBasicMaterial({ 
+        color: 0x00ff00 // green 
+    }); 
+    var mesh = new THREE.Mesh(geometry, material); 
+    return mesh; 
+}
+
+function getPlane(size){ 
+    var geometry = new THREE.PlaneGeometry(size, size); 
+    var material = new THREE.MeshBasicMaterial({ 
+        color: "red", 
+        side: THREE.DoubleSide
+    }); 
+    var mesh = new THREE.Mesh(geometry, material); 
+    return mesh; 
+}
 
 
-
+var scene = init(); 
