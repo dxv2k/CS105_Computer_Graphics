0015d1e07bef1957482a608962241358b9dbcc6f
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-06-01 17:40:54 +0700

    add GUI lib

--- threejs_LAB/js/main.js
+++ threejs_LAB/js/main.js
@@ -2,12 +2,17 @@
 
 function init(){ 
     var scene = new THREE.Scene();
+    var enableFog = false; 
 
-    // Add fog to scene 
-    scene.fog = new THREE.FogExp2(0xfffff, 0.2); // param: color, density
-
+    if (enableFog){ 
+        // Add fog to scene 
+        scene.fog = new THREE.FogExp2(0xfffff, 0.2); // param: color, density
+    }
+    
     var box = getBox(1,1,1);
     var plane = getPlane(20);
+    var pointLight = getPointLight(1);
+    var sphere = getSphere(0.05);
 
     // Set object as name and we can find it, call it by name
     plane.name = 'plane-1'; 
@@ -15,10 +20,14 @@ function init(){
     box.position.y = box.geometry.parameters.height/2; // move box on top of plane  
     plane.rotation.x = Math.PI/2; // view at 90 degree angle 
     // plane.rotation.y = 1;  
+    pointLight.position.y = 2; 
+
 
     scene.add(box); 
     // plane.add(box); 
     scene.add(plane); 
+    pointLight.add(sphere);
+    scene.add(pointLight); 
 
     var camera = new THREE.PerspectiveCamera( 
         45, // field of view 
@@ -41,13 +50,20 @@ function init(){
     // renderer.setClearColor(0xffff); // set background color 
     // use css style for color setting 
 
-    renderer.setClearColor('rgb(255,255,255)'); // set background color 
+    // renderer.setClearColor('rgb(255,255,255)'); // set background color 
+    renderer.setClearColor('rgb(120,120,120)'); // set background color 
     // document.body.appendChild( renderer.domElement );
     document.getElementById('webgl').appendChild(renderer.domElement); 
     renderer.render(scene, camera);
     update(renderer,scene,camera); 
     return scene; 
 }
+
+function getPointLight(intensity){ 
+    var light = new THREE.PointLight(0xffffff, intensity); 
+    return light;  
+}
+
 
 function update(renderer, scene, camera){ 
     renderer.render(scene, camera);
@@ -68,10 +84,22 @@ function update(renderer, scene, camera){
     }); // get callback in recursive manner
 }
 
+function getSphere(size){ 
+    var geometry = new THREE.SphereGeometry(size, 24, 24); // 24,24 "smoothness" of surface 
+    var material = new THREE.MeshBasicMaterial({ 
+        // color: 0x00ff00 // green 
+        color: 'rgb(255,255,255)'
+    }); 
+    var mesh = new THREE.Mesh(geometry, material); 
+    return mesh; 
+}
+
+
 function getBox(w, h, d){ 
     var geometry = new THREE.BoxGeometry(w,h,d); 
-    var material = new THREE.MeshBasicMaterial({ 
-        color: 0x00ff00 // green 
+    var material = new THREE.MeshPhongMaterial({ 
+        // color: 0x00ff00 // green 
+        color: 'rgb(120,120,120)'
     }); 
     var mesh = new THREE.Mesh(geometry, material); 
     return mesh; 
@@ -79,8 +107,9 @@ function getBox(w, h, d){
 
 function getPlane(size){ 
     var geometry = new THREE.PlaneGeometry(size, size); 
-    var material = new THREE.MeshBasicMaterial({ 
-        color: "red", 
+    var material = new THREE.MeshPhongMaterial({ 
+        // color: "red", 
+        color: 'rgb(120,120,120)', 
         side: THREE.DoubleSide
     }); 
     var mesh = new THREE.Mesh(geometry, material); 
